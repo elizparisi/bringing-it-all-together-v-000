93cb0b3f8a862236580d4f096025247129acd76f
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-05-29 04:10:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,11 +22,11 @@ class Dog
   end
   
   def self.drop_table
-    sql = <<-SQL 
+    sql = <<-SQL
       DROP TABLE IF EXISTS dogs
     SQL
-    
-    DB:[:conn].execute(sql)
+
+    DB[:conn].execute(sql)
   end
 
 end
