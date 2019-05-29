22ad3eef36a577c29247937958a9b62325b74f22
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-05-29 04:46:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -104,7 +104,13 @@ class Dog
   end
   
   def update
-    
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
     
 end
