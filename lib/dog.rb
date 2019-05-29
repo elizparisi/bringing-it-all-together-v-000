2d2bc2edf1c9460583505f5e325f543fa11a80f6
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-05-29 04:37:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -98,8 +98,9 @@ class Dog
       LIMIT 1 
     SQL
     
-    DB[:conn].execute(sql, name)
-    self.new_from_db(row)
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
     
 end
