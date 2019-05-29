514ce66b7abdd5a30e73a67fc5069f9331d5d3a8
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-05-29 04:18:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,7 +46,6 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
-    dog
   end
   
   def self.find_by_name
